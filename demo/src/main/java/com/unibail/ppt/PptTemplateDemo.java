da26c9eced5fd660ebc5eecb664b79e83184f8f3
Aurélien Manteaux <amanteaux@coreoz.com>
2016-12-14 12:15:01 +0100

    Project demo separated from tests

---
+++ demo/src/main/java/com/unibail/ppt/PptTemplateDemo.java
@@ -0,0 +1,32 @@
+package com.unibail.ppt;
+
+import java.awt.Color;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.apache.poi.util.IOUtils;
+
+import com.coreoz.ppt.PptMapper;
+
+public class PptTemplateDemo {
+
+	public static void main(String[] args) throws IOException {
+		try(FileOutputStream out = new FileOutputStream("full_content_transformed.pptx")) {
+			new PptMapper()
+				.hide("hidden", arg -> "true".equals(arg))
+				.text("var1", "Content replaced")
+				.text("var3", "Header cell replaced")
+				.text("var4", "Content cell replaced")
+				.imageJpg("image", IOUtils.toByteArray(PptTemplateDemo.class.getResourceAsStream("/images/replacedImage.jpg")))
+				.styleText("style", textRun -> {
+					textRun.setBold(true);
+					textRun.setFontColor(Color.GREEN);
+				})
+				.processTemplate(PptTemplateDemo.class.getResourceAsStream("/full_content.pptx"))
+				.write(out);
+		}
+		System.out.println("Templated presentation generated in " + Paths.get("full_content_transformed.pptx").toAbsolutePath());
+	}
+
+}
