b4c8946153d8cba4b661468423fbbe5cada0dca6
Aurélien Manteaux <amanteaux@coreoz.com>
2016-12-14 12:05:20 +0100

    Add docs

--- src/main/java/com/coreoz/ppt/PptMapper.java
+++ src/main/java/com/coreoz/ppt/PptMapper.java
@@ -1,5 +1,6 @@
 package com.coreoz.ppt;
 
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -9,9 +10,17 @@ import java.util.function.Predicate;
 
 import org.apache.poi.sl.usermodel.PictureData.PictureType;
 import org.apache.poi.sl.usermodel.TextRun;
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
 
+/**
+ * Bind PowerPoint variables to actions :<br/>
+ * - text replacement,<br/>
+ * - image replacement,<br/>
+ * - text and shape styling,<br/>
+ * - text, shape and image hiding.<br/>
+ */
 public class PptMapper {
-	
+
 	private final Map<String, PptTextMapper> textMapping;
 	private final Map<String, PptImageMapper> imageMapping;
 	private final Map<String, PptHidingMapper> hideMapping;
@@ -23,39 +32,120 @@ public class PptMapper {
 		this.hideMapping = new HashMap<>();
 		this.styleTextMapping = new HashMap<>();
 	}
-	
+
+	// configuration
+
+	/**
+	 * Replace a text variable with a value.
+	 *
+	 * @param variableName The variable name.
+	 * It should be in the form of <code>$/variableName/</code> in the PPT presentation
+	 * @param value The value that will replaced the variable.
+	 * {@link Object#toString()}} will be called upon the object.
+	 * If the Object is null, then the variable will be replaced by an empty String
+	 * @return The mapper instance
+	 */
 	public PptMapper text(String variableName, Object value) {
 		textMapping.put(variableName, PptTextMapper.of(value, null));
 		return this;
 	}
-	
+
+	/**
+	 * Replace an image placeholder by an other image.
+	 * This image placeholder is identified with a link placed on it.
+	 * To fit the placeholder, the replacement image will be resized and truncated
+	 * relatively to its center.
+	 *
+	 * @param variableName The variable name.
+	 * It should be in the form of <code>$/variableName:'argument'/</code> in the PPT presentation
+	 * @param imageData The raw data of the JPG image that will replace the placeholder
+	 * @return The mapper instance
+	 */
 	public PptMapper imageJpg(String variableName, byte[] imageData) {
 		imageMapping.put(variableName, PptImageMapper.of(PictureType.JPEG, imageData));
 		return this;
 	}
-	
+
+	/**
+	 * Remove from the presentation a text, a shape or an image
+	 * based on a variable placed in the link of the element.
+	 *
+	 * @param variableName The variable name.
+	 * It should be in the form of <code>$/variableName:'argument'/</code> in the PPT presentation
+	 * @return The mapper instance
+	 */
 	public PptMapper hide(String variableName) {
 		hideMapping.put(variableName, PptHidingMapper.of(arg -> true));
 		return this;
 	}
-	
+
+	/**
+	 * Remove from the presentation a text, a shape or an image
+	 * based on a variable placed in the link of the element.
+	 *
+	 * @param variableName The variable name.
+	 * It should be in the form of <code>$/variableName:'argument'/</code> in the PPT presentation
+	 * @param shouldHide The predicate that accepts the variable argument
+	 * @return The mapper instance
+	 */
 	public PptMapper hide(String variableName, Predicate<String> shouldHide) {
 		hideMapping.put(variableName, PptHidingMapper.of(shouldHide));
 		return this;
 	}
-	
+
+	/**
+	 * Style a text in the presentation by directly modifying the {@link TextRun}
+	 * object provided by POI.
+	 * The text object is identified with a link placed on it.
+	 *
+	 * @param variableName The variable name.
+	 * It should be in the form of <code>$/variableName:'argument'/</code> in the PPT presentation
+	 * @param applyText The consumer that will directly change the {@link TextRun}
+	 * @return The mapper instance
+	 */
 	public PptMapper styleText(String variableName, Consumer<TextRun> applyText) {
 		styleTextMapping.put(variableName, PptStyleTextMapper.of((arg, textRun) -> applyText.accept(textRun)));
 		return this;
 	}
-	
+
+	/**
+	 * Style a text in the presentation by directly modifying the {@link TextRun}
+	 * object provided by POI.
+	 * The text object is identified with a link placed on it.
+	 *
+	 * @param variableName The variable name.
+	 * It should be in the form of <code>$/variableName:'argument'/</code> in the PPT presentation
+	 * @param applyText The bi consumer that will directly change the {@link TextRun},
+	 * the first consumer parameter is the variable argument
+	 * @return The mapper instance
+	 */
 	public PptMapper styleText(String variableName, BiConsumer<String, TextRun> applyText) {
 		styleTextMapping.put(variableName, PptStyleTextMapper.of(applyText));
 		return this;
 	}
-	
+
+	// helper
+
+	/**
+	 * Fill in the template with the mapper data.
+	 * @see PptTemplates#process(InputStream, PptMapper)
+	 */
+	public XMLSlideShow processTemplate(InputStream templateData) {
+		return PptTemplates.process(templateData, this);
+	}
+
+	/**
+	 * Fill in the template with the mapper data.
+	 * The template passed as a parameter will directly be modified.
+	 * @see PptTemplates#process(InputStream, PptMapper)
+	 * @return The template passed as a parameter
+	 */
+	public XMLSlideShow processTemplate(XMLSlideShow templateData) {
+		return PptTemplates.processPpt(templateData, this);
+	}
+
 	// package API
-	
+
 	Optional<String> textMapping(String variableName) {
 		return Optional
 			.ofNullable(textMapping.get(variableName))
@@ -74,7 +164,7 @@ public class PptMapper {
 			.ofNullable(hideMapping.get(variableName))
 			.map(hidingMapper -> hidingMapper.getShouldHide().test(argument));
 	}
-	
+
 	Optional<BiConsumer<String, TextRun>> styleText(String variableName) {
 		return Optional
 			.ofNullable(styleTextMapping.get(variableName))
@@ -82,9 +172,9 @@ public class PptMapper {
 	}
 
 	// internal
-	
+
 	private static Object nullToEmpty(Object value) {
 		return value == null ? "" : value;
 	}
-	
+
 }
--- src/main/java/com/coreoz/ppt/PptTemplates.java
+++ src/main/java/com/coreoz/ppt/PptTemplates.java
@@ -30,42 +30,63 @@ import net.coobird.thumbnailator.Thumbnails;
 import net.coobird.thumbnailator.geometry.Positions;
 
 /**
- * Variable format in the PPT: $/variableName:'argument'/
+ * Enable to update PowerPoint presentation with dynamic data via a variable system.<br/>
+ * <br/>
+ * Variable format in the PPT is always: <code>$/variableName:'argument'/</code><br/>
+ * <br/>
+ * <strong>Be aware that the template presentation should NOT
+ * include any SmartArt or any Excel graphic.
+ * This would very likely lead to a corrupted presentation.</strong>
  */
 public class PptTemplates {
 
+	/**
+	 * Fill in the template with the mapper data.
+	 * @param templateData The stream to the template data
+	 * @param mapper The object used to fill in the template
+	 * @return A PowerPoint presentation filled with data
+	 */
 	@SneakyThrows
-	public static XMLSlideShow process(InputStream pptData, PptMapper mapper) {
-		XMLSlideShow ppt = new XMLSlideShow(pptData);
-		
+	public static XMLSlideShow process(InputStream templateData, PptMapper mapper) {
+		XMLSlideShow ppt = new XMLSlideShow(templateData);
+
 		processPpt(ppt, mapper);
-			
+
 		return ppt;
 	}
-	
-	public static void processPpt(XMLSlideShow ppt, PptMapper mapper) {
+
+	/**
+	 * Fill in the template with the mapper data.
+	 * The template passed as a parameter will directly be modified.
+	 * @param ppt The template presentation to be filled
+	 * @param mapper The object used to fill in the template
+	 * @return The template passed as a parameter
+	 */
+	public static XMLSlideShow processPpt(XMLSlideShow ppt, PptMapper mapper) {
 		for(XSLFSlide slide : ppt.getSlides()) {
 			List<ImageToReplace> imagesToReplace = new ArrayList<>();
 			List<XSLFShape> shapesToDelete = new ArrayList<>();
-			
+
 			for(XSLFShape shape : slide.getShapes()) {
 				if(processShape(imagesToReplace, shape, mapper)) {
 					shapesToDelete.add(shape);
 				}
 			}
-			
+
 			for(XSLFShape shapeToDelete : shapesToDelete) {
 				slide.removeShape(shapeToDelete);
 			}
-			
+
 			for(ImageToReplace imageToReplace : imagesToReplace) {
 				replaceImage(ppt, slide, imageToReplace);
 			}
 		}
+
+		return ppt;
 	}
-	
+
 	// internal
-	
+
 	/**
 	 * Handles shape modification
 	 * @return true is the shape should be removed
@@ -82,22 +103,22 @@ public class PptTemplates {
 		}
 		return false;
 	}
-	
+
 	private static boolean processImageShape(List<ImageToReplace> imagesToReplace, XSLFPictureShape imageShape, PptMapper mapper) {
 		Optional<PptVariable> imageVariable = parseHyperlinkVariale(imageShape);
 		if(shouldHide(imageVariable, mapper)) {
 			return true;
 		}
-		
+
 		imageVariable
 			.flatMap(variable -> mapper.imageMapping(variable.getName()))
 			.ifPresent(imageMapper -> {
 				imagesToReplace.add(ImageToReplace.of(imageShape, imageMapper));
 			});
-		
+
 		return false;
 	}
-	
+
 	private static void replaceImage(XMLSlideShow ppt, XSLFSlide slide, ImageToReplace imageToReplace) {
 		byte[] newPictureResized = resizeImage(
 			imageToReplace.imageMapper.getValue(),
@@ -108,39 +129,39 @@ public class PptTemplates {
 		XSLFPictureData newPictureData = ppt.addPicture(newPictureResized, imageToReplace.imageMapper.getTargetFormat());
 		XSLFPictureShape newPictureShape = slide.createPicture(newPictureData);
 		newPictureShape.setAnchor(imageToReplace.toReplace.getAnchor());
-		slide.removeShape(imageToReplace.toReplace); 
+		slide.removeShape(imageToReplace.toReplace);
 	}
-	
+
 	@SneakyThrows
 	private static byte[] resizeImage(byte[] imageData, String targetFormat, int width, int height) {
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 		Thumbnails
 			.of(new ByteArrayInputStream(imageData))
-            .crop(Positions.CENTER)
-            .outputQuality(1F)
-            .size(width, height)
-            .outputFormat(targetFormat)
-            .toOutputStream(byteArrayOutputStream);
-        return byteArrayOutputStream.toByteArray();
+			.crop(Positions.CENTER)
+			.outputQuality(1F)
+			.size(width, height)
+			.outputFormat(targetFormat)
+			.toOutputStream(byteArrayOutputStream);
+		return byteArrayOutputStream.toByteArray();
 	}
-	
+
 	private static boolean processTableShape(XSLFTable tableShape, PptMapper mapper) {
 		for(XSLFTableRow row : tableShape.getRows()) {
 			for(XSLFTableCell cell : row.getCells()) {
 				processTextParagraphs(cell.getTextParagraphs(), mapper);
 			}
 		}
-		
+
 		return false;
 	}
-	
+
 	private static boolean processTextShape(XSLFTextShape textShape, PptMapper mapper) {
 		if(shouldHide(textShape, mapper)) {
 			return true;
 		}
-		
+
 		processTextParagraphs(textShape.getTextParagraphs(), mapper);
-		
+
 		return false;
 	}
 
@@ -148,38 +169,38 @@ public class PptTemplates {
 		for (XSLFTextParagraph paragraph : paragraphs) {
 			for (XSLFTextRun textRun : paragraph.getTextRuns()) {
 				Optional<PptVariable> parsedHyperlinkVariale = parseHyperlinkVariale(textRun.getHyperlink());
-				
+
 				parsedHyperlinkVariale
 					.flatMap(variable -> mapper.styleText(variable.getName()))
 					.ifPresent(styler -> styler.accept(parsedHyperlinkVariale.get().getArg1(), textRun));
-				
+
 				if(shouldHide(parsedHyperlinkVariale, mapper)) {
-					textRun.setText("");	
+					textRun.setText("");
 				} else if(parsedHyperlinkVariale.isPresent()) {
 					textRun.getXmlObject().getRPr().unsetHlinkClick();
 				}
 			}
-			
+
 			PptParser.replaceTextVariable(paragraph, mapper);
 		}
 	}
-	
+
 	private static boolean shouldHide(SimpleShape<?, ?> simpleShape, PptMapper mapper) {
 		Optional<PptVariable> parsedHyperlinkVariale = parseHyperlinkVariale(simpleShape);
-		
+
 		// if the link is a variable, remove the link
 		parsedHyperlinkVariale.ifPresent(variable -> {
 			XSLFShape shape = (XSLFShape) simpleShape;
-			
+
 			String xquery = "declare namespace p='http://schemas.openxmlformats.org/presentationml/2006/main' .//*/p:cNvPr";
-            XmlObject[] rs = shape.getXmlObject().selectPath(xquery);
-            CTNonVisualDrawingProps nvPr = (CTNonVisualDrawingProps) rs[0];
-            nvPr.unsetHlinkClick();
+			XmlObject[] rs = shape.getXmlObject().selectPath(xquery);
+			CTNonVisualDrawingProps nvPr = (CTNonVisualDrawingProps) rs[0];
+			nvPr.unsetHlinkClick();
 		});
-		
+
 		return shouldHide(parsedHyperlinkVariale, mapper);
 	}
-	
+
 	private static boolean shouldHide(Optional<PptVariable> variable, PptMapper mapper) {
 		return variable
 			.flatMap(shapeVariable ->
@@ -187,23 +208,23 @@ public class PptTemplates {
 			)
 			.orElse(false);
 	}
-	
+
 	private static Optional<PptVariable> parseHyperlinkVariale(SimpleShape<?, ?> simpleShape) {
 		return parseHyperlinkVariale(simpleShape.getHyperlink());
 	}
-	
-	
+
+
 	private static Optional<PptVariable> parseHyperlinkVariale(Hyperlink<?, ?> link) {
 		if(link != null && link.getTypeEnum() == HyperlinkType.URL) {
 			return PptParser.parse(link.getAddress());
 		}
 		return Optional.empty();
 	}
-	
+
 	@Value(staticConstructor = "of")
 	private static class ImageToReplace {
 		private final XSLFPictureShape toReplace;
 		private final PptImageMapper imageMapper;
 	}
-	
+
 }
